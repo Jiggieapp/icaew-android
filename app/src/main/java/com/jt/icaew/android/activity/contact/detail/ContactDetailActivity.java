521905834eb18295af15ac97adb2b53ecef724b6
aga42 <itu.aga42@gmail.com>
2016-08-09 14:55:11 +0700

    recycleview android

--- app/src/main/java/com/jt/icaew/android/activity/contact/detail/ContactDetailActivity.java
+++ app/src/main/java/com/jt/icaew/android/activity/contact/detail/ContactDetailActivity.java
@@ -3,16 +3,18 @@ package com.jt.icaew.android.activity.contact.detail;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
 import com.jt.icaew.android.R;
 import com.jt.icaew.android.activity.BaseActivity;
 import com.jt.icaew.android.activity.contact.ContactPresenterImplementation;
 import com.jt.icaew.android.activity.contact.ContactView;
+import com.jt.icaew.android.activity.contact.adapter.ContactListAdapter;
 import com.jt.icaew.android.network.contact.ContactDetailResult;
 import com.jt.icaew.android.utils.Constant;
 
@@ -22,36 +24,17 @@ import butterknife.ButterKnife;
 /**
  * Created by Wandy on 7/13/2016.
  */
-public class ContactDetailActivity extends BaseActivity implements ContactView.OnFinishGetContactDetailListener{
+public class ContactDetailActivity extends BaseActivity implements ContactView.OnFinishGetContactDetailListener, ContactListAdapter.OnContactListSelectedListener {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
-
-    @BindView(R.id.lbl_university_address)
-    TextView lblUniversityAddress;
-
-    @BindView(R.id.lbl_university_email)
-    TextView lblUniversityEmail;
-
-    @BindView(R.id.lbl_university_facebook)
-    TextView lblUniversityFacebook;
-
-    @BindView(R.id.lbl_university_name)
-    TextView lblUniversityName;
-
-    @BindView(R.id.lbl_university_phone)
-    TextView lblUniversityPhone;
-
-    @BindView(R.id.lbl_university_website)
-    TextView lblUniversityWebsite;
-
-    @BindView(R.id.img_banner)
-    ImageView imgBanner;
 
     private final String TAG = ContactDetailActivity.class.getSimpleName();
     ContactPresenterImplementation implementation;
 
     long countryId;
+    @BindView(R.id.recycler_events)
+    RecyclerView recyclerEvents;
 
     @Override
     public void onCreate() {
@@ -59,11 +42,9 @@ public class ContactDetailActivity extends BaseActivity implements ContactView.O
         ButterKnife.bind(this);
         Bundle bundle = getBundle();
 
-        if(toolbar != null)
-        {
+        if (toolbar != null) {
             setSupportActionBar(toolbar);
-            if(bundle != null)
-            {
+            if (bundle != null) {
                 countryId = bundle.getLong(Constant.PARAM_COUNTRY_ID);
                 final String countryName = bundle.getString(Constant.PARAM_COUNTRY_NAME);
                 getSupportActionBar().setTitle(countryName);
@@ -77,11 +58,14 @@ public class ContactDetailActivity extends BaseActivity implements ContactView.O
         implementation.setOnFinishGetContactDetailListener(this);
         showProgressDialog();
         implementation.getContactUsDetail(countryId + "");
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerEvents.setLayoutManager(layoutManager);
     }
 
     @Override
     public void onFinishGetContactDetail(ContactDetailResult contactDetailResult) {
-        final ContactDetailResult.Data data = contactDetailResult.data.get(0);
+        /*final ContactDetailResult.Data data = contactDetailResult.data.get(0);
         if(data.getImage().isEmpty())
         {
             Glide.with(this).load(R.drawable.default_banner_bg).into(imgBanner);
@@ -95,8 +79,16 @@ public class ContactDetailActivity extends BaseActivity implements ContactView.O
         lblUniversityEmail.setText(data.getEmail());
         lblUniversityPhone.setText(data.getTelp());
         lblUniversityFacebook.setText(data.getFacebook());
-        lblUniversityWebsite.setText(data.getWebsite());
+        lblUniversityWebsite.setText(data.getWebsite());*/
+
+        ContactListAdapter adapter = new ContactListAdapter(ContactDetailActivity.this, contactDetailResult.getData(), this);
+        recyclerEvents.setAdapter(adapter);
         dismissProgressDialog();
+    }
+
+    @Override
+    public void onContactListSelected(ContactDetailResult.Data data) {
+
     }
 
     @Override
@@ -106,22 +98,20 @@ public class ContactDetailActivity extends BaseActivity implements ContactView.O
         finish();
     }
 
-    private Context getContext()
-    {
+    private Context getContext() {
         return getActivityController().getContext();
     }
 
     ProgressDialog progressDialog;
-    private void showProgressDialog()
-    {
+
+    private void showProgressDialog() {
         progressDialog = ProgressDialog.show(this, "", getResources().getString(R.string.please_wait));
         progressDialog.setCancelable(false);
         progressDialog.show();
     }
 
-    private void dismissProgressDialog()
-    {
-        if(progressDialog != null && progressDialog.isShowing())
+    private void dismissProgressDialog() {
+        if (progressDialog != null && progressDialog.isShowing())
             progressDialog.dismiss();
     }
 
